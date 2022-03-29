d11f319289d1840f54f656356b4009205f7a06ae
Zijian-Wang <ryegogogo@gmail.com>
2022-03-29 23:01:45 +0800

    Lesson 16 Part 2
    Complete shadows and learn about baked shadow maps' usage.

--- Class 15-lights/src/script.js
+++ Class 15-lights/src/script.js
@@ -3,6 +3,9 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
+import { PCFShadowMap, PCFSoftShadowMap } from 'three'
+
+
 
 /**
  * Base
@@ -19,33 +22,45 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight()
-ambientLight.color = new THREE.Color('white')
-ambientLight.intensity = 0.3
-scene.add(ambientLight)
+// const ambientLight = new THREE.AmbientLight()
+// ambientLight.color = new THREE.Color('white')
+// ambientLight.intensity = 0.3
+// scene.add(ambientLight)
 
-const areaLight = new THREE.RectAreaLight(0xffffff, 2, 1, 1)
-areaLight.position.set( 1, 1, 1)
-areaLight.lookAt(new THREE.Vector3(0, 0, 0))
-scene.add(areaLight)
-// areaLight.castShadow = true
-const arealightHelper = new RectAreaLightHelper(areaLight)
-scene.add(arealightHelper)
+// const areaLight = new THREE.RectAreaLight(0xffffff, 2, 1, 1)
+// areaLight.position.set( 1, 1, 1)
+// areaLight.lookAt(new THREE.Vector3(0, 0, 0))
+// scene.add(areaLight)
+// // areaLight.castShadow = true
+// const arealightHelper = new RectAreaLightHelper(areaLight)
+// scene.add(arealightHelper)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.width = 2048
-directionalLight.shadow.mapSize.height = 2048
-directionalLight.shadow.blurSamples = 32
-scene.add(directionalLight)
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+// directionalLight.castShadow = true
+// directionalLight.shadow.mapSize.width = 2048
+// directionalLight.shadow.mapSize.height = 2048
+// directionalLight.shadow.radius = 5
+// directionalLight.shadow.blurSamples = 32
+// directionalLight.shadow.camera.far = 2
+// directionalLight.shadow.camera.top = 2
+// directionalLight.shadow.camera.left = -2
+// directionalLight.shadow.camera.right = 2
+// directionalLight.shadow.camera.bottom = -2
+// scene.add(directionalLight)
 
-const spotLight = new THREE.SpotLight(0xffffff, 0.5, 5, Math.PI*0.15, 1, 1)
+// const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(directionalLightCameraHelper)
+
+const spotLight = new THREE.SpotLight(0xffffff, 2, 8, Math.PI*0.3, 1, 1)
 spotLight.position.set(0, 3, 3)
-spotLight.target.position.set(0, 0, 0)
+spotLight.target.position.set(0, 0, -3)
 spotLight.castShadow = true
 spotLight.shadow.mapSize.width = 2048
 spotLight.shadow.mapSize.height = 2048
+spotLight.shadow.radius = 5
 spotLight.shadow.blurSamples = 32
+spotLight.shadow.camera.far = 5
+spotLight.shadow.camera.near = 3
 // window.addEventListener('mousemove', (event) => {
 //     spotLight.target.position.x = (event.clientX/window.innerWidth - 0.5) * 10
 //     // spotLight.target.position.y = event.clientY
@@ -56,11 +71,25 @@ spotLight.shadow.blurSamples = 32
 scene.add(spotLight, spotLight.target)
 
 const spotlightHelper = new THREE.SpotLightHelper(spotLight)
-scene.add(spotlightHelper)
+const spotlightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+spotlightCameraHelper.visible = false
+spotlightHelper.visible = false
+scene.add(spotlightHelper, spotlightCameraHelper)
 window.requestAnimationFrame( () => {
     spotlightHelper.update()
 })
 
+const pointLight = new THREE.PointLight(0xffffff, 0.3, 8, 1)
+pointLight.position.set(1, 1, 0)
+pointLight.castShadow = true
+
+pointLight.shadow.mapSize.width = 2048
+pointLight.shadow.mapSize.height = 2048
+pointLight.shadow.radius = 32
+pointLight.shadow.camera.far = 8
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+pointLightCameraHelper.visible = false
+scene.add(pointLight, pointLightCameraHelper)
 
 // const shadowMap = {
 //     resolution: 0
@@ -155,7 +184,8 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.shadowMap.enabled = true
+renderer.shadowMap.enabled = false
+// renderer.shadowMap.type = PCFSoftShadowMap
 
 /**
  * Animate
